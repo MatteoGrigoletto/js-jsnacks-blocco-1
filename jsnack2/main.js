dbf78448cd8d1bb964ed2d8881c9be4d2b46def5
MatteoGrigoletto <mttgrigoletto@gmail.com>
2022-10-26 10:58:17 +0200

    d

--- jsnack2/main.js
+++ jsnack2/main.js
@@ -1,9 +1,10 @@
-let firstWord=prompt('scrivi una parola');
-let secondWord=prompt('scrivi una parola');
+let firstWord = prompt('scrivi una parola');
+let secondWord = prompt('scrivi una parola');
+
 if(firstWord.length<secondWord.length){
-    console.log("piu corta:",firstWord);
+    console.log(firstWord);
     console.log(secondWord);
 }else{
-    console.log("piu corta:",secondWord);
+    console.log(secondWord);
     console.log(firstWord);
 }
